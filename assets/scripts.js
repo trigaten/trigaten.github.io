a177e7269454403e3c9056fb9660be873a2a2ecd
trigaten <sanderman5@hotmail.com>
2021-07-07 16:20:28 -0400

    Site 1.9 (almost 2.0)

---
+++ assets/scripts.js
@@ -0,0 +1,11 @@
+function showCarouselItems(){
+    $(".click-hide").show();
+    document.getElementById("carousel").setAttribute("data-interval", "true") 
+}
+
+function hideCarouselItems(){
+    $(".click-hide").hide();
+    document.getElementById("carousel").setAttribute("data-interval", "false") 
+}
+
+
